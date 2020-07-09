ba7a406a6b64559666fc33fb1f0bd0387a66941d
Chris Ryan <info@chrisryan.tech>
2020-07-09 16:53:35 +0100

    removing ogr2org

--- CountriesVisted/settings.py
+++ CountriesVisted/settings.py
@@ -123,13 +123,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
