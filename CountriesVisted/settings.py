d6380ee0f0342fb9ef126d364f06cc27aa3f8bdc
Chris Ryan <info@chrisryan.tech>
2020-07-09 14:59:24 +0100

    updated settings file for GDAL

--- CountriesVisted/settings.py
+++ CountriesVisted/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
